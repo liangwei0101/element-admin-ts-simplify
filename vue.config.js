42076973467f25b5db0cae93d766733bf81e029a
liangwei0101 <liangwei0101@outlook.com>
2019-11-14 16:22:00 +0800

    frist commit

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const name = 'Vue Typescript Admin'
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-typescript-admin-template/' : '/', // TODO: Remember to change this to fit your need
+  lintOnSave: process.env.NODE_ENV === 'development',
+  pwa: {
+    name: name
+  },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, 'src/styles/_variables.scss'),
+        path.resolve(__dirname, 'src/styles/_mixins.scss')
+      ]
+    }
+  },
+  chainWebpack(config) {
+    // Provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    config.set('name', name)
+    config.module.rule('pug')
+      .test(/\.pug$/)
+      .use('pug-html-loader')
+      .loader('pug-html-loader')
+      .end()
+  }
+}
